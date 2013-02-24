097e624cdf68c3387abb2acddb0296584573a9a5
Rambo <rambo@rambo.co.za>
2013-02-24 14:23:41 +0200

    Quick update
    Basic stable version

--- com.rambo.ledcontroller.MainActivity/gen/com/rambo/ledcontroller/R.java
+++ com.rambo.ledcontroller.MainActivity/gen/com/rambo/ledcontroller/R.java
@@ -13,18 +13,29 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int led_icon=0x7f020002;
     }
     public static final class id {
-        public static final int close=0x7f080004;
+        public static final int btOnOff=0x7f080005;
+        public static final int close=0x7f080003;
         public static final int entry=0x7f080001;
         public static final int label=0x7f080000;
-        public static final int menu_settings=0x7f080009;
-        public static final int open=0x7f080002;
-        public static final int seekBar1=0x7f080005;
-        public static final int seekBar2=0x7f080008;
-        public static final int seekBar3=0x7f080007;
-        public static final int seekBar4=0x7f080006;
-        public static final int send=0x7f080003;
+        public static final int linearLayout1=0x7f08000a;
+        public static final int menu_settings=0x7f080013;
+        public static final int mode1=0x7f08000b;
+        public static final int mode2=0x7f08000c;
+        public static final int mode3=0x7f08000d;
+        public static final int mode4=0x7f08000e;
+        public static final int mode5=0x7f08000f;
+        public static final int mode6=0x7f080010;
+        public static final int mode7=0x7f080011;
+        public static final int mode8=0x7f080012;
+        public static final int open=0x7f080004;
+        public static final int seekBlue=0x7f080009;
+        public static final int seekBrightness=0x7f080006;
+        public static final int seekGreen=0x7f080008;
+        public static final int seekRed=0x7f080007;
+        public static final int send=0x7f080002;
     }
     public static final class integer {
         public static final int color_max=0x7f040000;
@@ -37,13 +48,25 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int btOff=0x7f050007;
+        public static final int btOn=0x7f050006;
         public static final int menu_settings=0x7f050001;
+        public static final int mode=0x7f050008;
+        public static final int mode1=0x7f050009;
+        public static final int mode2=0x7f05000a;
+        public static final int mode3=0x7f05000b;
+        public static final int mode4=0x7f05000c;
+        public static final int mode5=0x7f05000d;
+        public static final int mode6=0x7f05000e;
+        public static final int mode7=0x7f05000f;
+        public static final int mode8=0x7f050010;
         public static final int sb_name_blue=0x7f050003;
         public static final int sb_name_green=0x7f050005;
         public static final int sb_name_red=0x7f050004;
         public static final int title_activity_main=0x7f050002;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f060001;
+        public static final int modes=0x7f060000;
     }
 }
--- com.rambo.ledcontroller.MainActivity/src/com/rambo/ledcontroller/MainActivity.java
+++ com.rambo.ledcontroller.MainActivity/src/com/rambo/ledcontroller/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
@@ -22,10 +23,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	// Constants:
+	//Suit Modes:
+	private static final int WHITE = 1;
+	private static final int RED = 5;
+	private static final int GREEN = 6;
+	private static final int BLUE = 7;
+	private static final int MUSIC = 13;
+	private static final int RAINBOW = 17;
+	private static final int STROBE = 12;
+	private static final int PIXFLASH = 14;
+	private static final int SIN = 15;
+	private static final int SHOOTINGRAINBOW = 16;
+	
+	//Flags:	
+	boolean enabled_Flag = false;
+	boolean open_Flag = false;
+	boolean led_Flag = true;
 
-    TextView myLabel;
+    TextView myLabel, mode1, mode2, mode3, mode4, mode5, mode6, mode7, mode8;
     EditText myTextbox;
-    SeekBar mySeekBar;
+    SeekBar mySeekBar, seekRed, seekGreen, seekBlue;
     BluetoothAdapter mBluetoothAdapter;
     BluetoothSocket mmSocket;
     BluetoothDevice mmDevice;
@@ -42,12 +61,97 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        //Find Buttons:
+        
         Button openButton = (Button)findViewById(R.id.open);
         Button sendButton = (Button)findViewById(R.id.send);
         Button closeButton = (Button)findViewById(R.id.close);
+        final Button OnOffButton = (Button)findViewById(R.id.btOnOff);
+        
+        //Find TextViews:
         myLabel = (TextView)findViewById(R.id.label);
+        mode1 = (TextView)findViewById(R.id.mode1);
+        mode2 = (TextView)findViewById(R.id.mode2);
+        mode3 = (TextView)findViewById(R.id.mode3);
+        mode4 = (TextView)findViewById(R.id.mode4);
+        mode5 = (TextView)findViewById(R.id.mode5);
+        mode6 = (TextView)findViewById(R.id.mode6);
+        mode7 = (TextView)findViewById(R.id.mode7);
+        mode8 = (TextView)findViewById(R.id.mode8);
+          
+       
+        
         myTextbox = (EditText)findViewById(R.id.entry);
-        mySeekBar = (SeekBar) findViewById(R.id.seekBar1);
+        
+        // Find SeekBars
+        mySeekBar = (SeekBar) findViewById(R.id.seekBrightness);
+        seekRed = (SeekBar) findViewById(R.id.seekRed);
+        seekGreen = (SeekBar) findViewById(R.id.seekGreen);
+        seekBlue = (SeekBar) findViewById(R.id.seekBlue);
+        
+        
+        // TextView OnClicks:
+        mode1.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				try {
+					sendData(MUSIC, 0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+        mode2.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				try {
+					sendData(RAINBOW, 0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+        mode3.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				try {
+					sendData(STROBE, 0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+        mode4.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				try {
+					sendData(PIXFLASH, 0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+        mode5.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				try {
+					sendData(SIN, 0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+        mode6.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				try {
+					sendData(SHOOTINGRAINBOW, 0);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+        
         
         //Open Button
         openButton.setOnClickListener(new View.OnClickListener()
@@ -57,7 +161,9 @@ public class MainActivity extends Activity {
                 try 
                 {
                     findBT();
-                    openBT();
+                    if (enabled_Flag){ // only try open if 'on'
+                    	openBT();
+                    }
                 }
                 catch (IOException ex) { }
             }
@@ -69,7 +175,7 @@ public class MainActivity extends Activity {
             public void onClick(View v)
             {
      
-            	\try 
+            	try 
                 {
                     sendData();
                 }
@@ -89,14 +195,47 @@ public class MainActivity extends Activity {
                 catch (IOException ex) { }
             }
         });
+
+        //OnOff button
+        OnOffButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+            	if(led_Flag){ //turn led's off
+            		try {
+    					sendData(WHITE, 0);
+    				} catch (IOException e) {
+    					// TODO Auto-generated catch block
+    					e.printStackTrace();
+    				}
+            		led_Flag = false;
+            		OnOffButton.setText(R.string.btOn);
+            		
+            	}else{ // turn led's on
+            		try {
+    					sendData(WHITE, 50);
+    				} catch (IOException e) {
+    					// TODO Auto-generated catch block
+    					e.printStackTrace();
+    				}
+            		led_Flag = true;
+            		OnOffButton.setText(R.string.btOff);
+            		
+            	}
+
+            }
+        });
+        
+        
+        
+        // SeekBar listeners: 
         
         mySeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
     		public void onProgressChanged(SeekBar seekBar, int progress,
     				boolean fromUser) {
     			
     			try {
-    				int white = 1;
-					sendData(white, progress);
+					sendData(WHITE, progress);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -111,7 +250,79 @@ public class MainActivity extends Activity {
     		public void onStopTrackingTouch(SeekBar seekBar) {
     			// TODO Auto-generated method stub
     		}
-    	});
+        });
+    		
+        seekRed.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+        	
+        		public void onProgressChanged(SeekBar seekBar, int progress,
+        				boolean fromUser) {
+        			
+        			try {
+    					sendData(RED, progress);
+    				} catch (IOException e) {
+    					// TODO Auto-generated catch block
+    					e.printStackTrace();
+    				}
+        			
+        		}
+
+        		public void onStartTrackingTouch(SeekBar seekBar) {
+        			// TODO Auto-generated method stub
+        		}
+
+        		public void onStopTrackingTouch(SeekBar seekBar) {
+        			// TODO Auto-generated method stub
+        		}
+        		
+        });
+
+        seekGreen.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+    		public void onProgressChanged(SeekBar seekBar, int progress,
+    				boolean fromUser) {
+    			
+    			try {
+					sendData(GREEN, progress);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    			
+    		}
+
+    		public void onStartTrackingTouch(SeekBar seekBar) {
+    			// TODO Auto-generated method stub
+    		}
+
+    		public void onStopTrackingTouch(SeekBar seekBar) {
+    			// TODO Auto-generated method stub
+    		}
+    		
+    });
+    
+        seekBlue.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+    		public void onProgressChanged(SeekBar seekBar, int progress,
+    				boolean fromUser) {
+    			
+    			try {
+					sendData(BLUE, progress);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    			
+    		}
+
+    		public void onStartTrackingTouch(SeekBar seekBar) {
+    			// TODO Auto-generated method stub
+    		}
+
+    		public void onStopTrackingTouch(SeekBar seekBar) {
+    			// TODO Auto-generated method stub
+    		}
+    		
+    });
+    
     }
 
     @Override
@@ -119,6 +330,51 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    @Override
+    public void onStart(){
+    	super.onStart();
+    	
+    	// check if we have an active device
+    	if(enabled_Flag){
+    		try {
+				openBT();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}else{
+    		findBT();
+            if (enabled_Flag){ // only try open if 'on'
+            	try {
+					openBT();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+            }
+    	}
+    }
+    
+    @Override
+	public void onStop() {
+    	super.onStop();
+    	
+    	// clean up a bit
+    	if (!stopWorker){
+	    	stopWorker = true;
+	    	try{
+		        mmOutputStream.close();
+		        mmInputStream.close();
+		        mmSocket.close();
+		        open_Flag = false;
+	    	}catch(Exception ex){
+	    		
+	    	}
+    	}
+    	
+    	
+    }
+    
+
     
     void findBT()
     {
@@ -140,6 +396,7 @@ public class MainActivity extends Activity {
         	Toast.makeText(this,
 					"Please enable your BT and re-run this program.",
 					Toast.LENGTH_LONG).show();
+        	enabled_Flag = false;
 			return;
         }
         
@@ -151,11 +408,13 @@ public class MainActivity extends Activity {
                 if(device.getName().equals("linvor")) 
                 {
                     mmDevice = device;
+                    myLabel.setText("Bluetooth Device Found: linvor");
                     break;
                 }
             }
         }
-        myLabel.setText("Bluetooth Device Found");
+        myLabel.setText("Bluetooth Device NOT Found!");
+        enabled_Flag = true;
     }
     
     void openBT() throws IOException
@@ -171,6 +430,7 @@ public class MainActivity extends Activity {
         myLabel.setText("Bluetooth Opened");
         Toast.makeText(this, "Connected!! :D",
 				Toast.LENGTH_LONG).show();
+        open_Flag = true;
     }
     
     void beginListenForData()
@@ -233,28 +493,35 @@ public class MainActivity extends Activity {
     
     void sendData() throws IOException
     {
-        String msg = myTextbox.getText().toString();
-        msg += "\n";
-        mmOutputStream.write(msg.getBytes());
-        myLabel.setText("Data Sent");
+    	if(open_Flag)
+    	{
+	        String msg = myTextbox.getText().toString();
+	        msg += "\n";
+	        mmOutputStream.write(msg.getBytes());
+	        myLabel.setText("Data Sent");
+    	}
     }
     
     void sendData(int data) throws IOException
     {
-        String msg = intToLZstr(data);
-        msg += "\n";
-        mmOutputStream.write(msg.getBytes());
-        myLabel.setText("Data Sent");
+    	if(open_Flag){
+	        String msg = intToLZstr(data);
+	        msg += "\n";
+	        mmOutputStream.write(msg.getBytes());
+	        myLabel.setText("Data Sent");
+	    }
     }
     
     void sendData(int setting, int data) throws IOException // configure a setting with certain data on the device
     {
-        String msg = intToLZstr(setting); // setting value
-        msg += ','; // delimiter
-        msg += intToLZstr(data); // data value for setting
-        msg += "\n"; // end of line delimiter
-        mmOutputStream.write(msg.getBytes());
-        myLabel.setText("LED setting: " + setting + " changed");
+    	if(open_Flag){
+	        String msg = intToLZstr(setting); // setting value
+	        msg += ','; // delimiter
+	        msg += intToLZstr(data); // data value for setting
+	        msg += "\n"; // end of line delimiter
+	        mmOutputStream.write(msg.getBytes());
+	        myLabel.setText("LED setting: " + setting + " changed");
+	    }
     }
     
 	public String intToLZstr(int text) { // should convert an int to a 3 digit string, 000 - 999?
@@ -279,6 +546,7 @@ public class MainActivity extends Activity {
         mmOutputStream.close();
         mmInputStream.close();
         mmSocket.close();
+        enabled_Flag = false;
         myLabel.setText("Bluetooth Closed");
     }
 }
